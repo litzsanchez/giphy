093002582f8dbc75feff16f74c251c17f1bcf889
HaCkEd <litzsanchez@ccpaedu.com>
2019-03-08 02:29:02 +0000

    Your message here

--- js/script.js
+++ js/script.js
@@ -4,7 +4,16 @@
 /* global $ */
 
 $("#search-button").click(function(){
-  
+          $.ajax({
+        url: "https://api.giphy.com/v1/gifs/search?q=puppy&rating=pg&api_key=dc6zaTOxFJmzC",
+        method: "GET",
+        success: function(response) {
+            // YOUR CODE GOES HERE
+        //console.log(response.data[2].images[2]);
+        var pic = response.data[0].url
+        $("container").append("<img src='" + pic + "'/>");
+        }
+    }); 
   
   
 });
