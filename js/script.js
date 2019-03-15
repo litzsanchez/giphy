86343da99551ab451365a3c48ee7287080a2697e
HaCkEd <litzsanchez@ccpaedu.com>
2019-03-15 01:24:53 +0000

    Your message here

--- js/script.js
+++ js/script.js
@@ -3,18 +3,27 @@
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 /* global $ */
 
+
+     
 $("#search-button").click(function(){
-          $.ajax({
-        url: "https://api.giphy.com/v1/gifs/search?q=puppy&rating=pg&api_key=dc6zaTOxFJmzC",
-        method: "GET",
-        success: function(response) {
-            // YOUR CODE GOES HERE
-        //console.log(response.data[2].images[2]);
-        var pic = response.data[0].url
-        $("container").append("<img src='" + pic + "'/>");
-        }
+$('.gallery').empty();
+var searchTerm = $('input').val();
+var request_url = "https://api.giphy.com/v1/gifs/search?q=" + searchTerm + "g&rating=pg&api_key=dc6zaTOxFJmzC";     
+          
+    $.ajax({
+    url: request_url,
+    method: "GET",
+    success: function(response) {
+        //     // YOUR CODE GOES HERE
+        // //console.log(response.data[2].images[2]);
+        // var pic = response.data[0].url
+
+         var pic_url = response.data[0].images.original.url;
+     $('.gallery').append('<img src="' + pic_url + '" />')
+      },
+
+
+  
     }); 
-  
-  
 });
 
